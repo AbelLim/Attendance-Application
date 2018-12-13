d3fc4789c5ee65feab00d9276ed8575ed43e5182
Vaibhav <vaibhav1andonlyone@yahoo.in>
2018-12-13 14:55:05 +0800

    test v

--- app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
+++ app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView textView;
     private Button testButton;
     private Database database;
-    private String string;
+    private String stringer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
