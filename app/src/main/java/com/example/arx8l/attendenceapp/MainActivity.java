76297e8b15d5c469f8271d19c6ea11bd52281b48
daoduytung2209 <daoduytung2209@gmail.com>
2018-12-15 00:26:42 +0800

    Add tap in, tap out features

--- app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
+++ app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
@@ -1,31 +1,87 @@
 package com.example.arx8l.attendenceapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+        implements MainScreenFragment.OnFragmentInteractionListener {
 
-    private TextView textView;
-    private Button testButton;
     private Database database;
-    private String stringer;
+
+    ImageView settings;
+    ImageView tapInTapOut;
+    ImageView checkAttendance;
+    ImageView medical;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         database = new Database();
-        textView = (TextView) findViewById(R.id.textView);
-        testButton = (Button) findViewById(R.id.testButton);
-        testButton.setOnClickListener(this);
+
+//        Intent loginIntent = new Intent(this, LoginActivity.class);
+//        startActivity(loginIntent);
+
+        checkAttendance = findViewById(R.id.check_attendance);
+        tapInTapOut = findViewById(R.id.tap_in_tap_out);
+        medical = findViewById(R.id.medical);
+        settings = findViewById(R.id.settings);
+
+        MainScreenFragment mainScreenFragment = new MainScreenFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_frag, mainScreenFragment, "");
+        fragmentTransaction.commit();
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        checkAttendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                CheckAttendanceFragment checkAttendanceFragment = new CheckAttendanceFragment();
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                fragmentTransaction.replace(R.id.main_frag, checkAttendanceFragment, "");
+//                fragmentTransaction.commit();
+            }
+        });
+
+
+        medical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                MedicalLeaveFragment medicalLeaveFragment = new MedicalLeaveFragment();
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                fragmentTransaction.replace(R.id.main_frag, medicalLeaveFragment, "");
+//                fragmentTransaction.commit();
+
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+//    @Override
+//    public void onClick(View v) {
         //Create new user
         /*database.createUser("12345678", "Jim", "Jim@my.jcu.edu.au", "password");*/
 
@@ -34,5 +90,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             textView.setText("Login Correct");
         else
             textView.setText("Login Failed");*/
-    }
+//    }
+
+
 }
---
+++ app/src/main/java/com/example/arx8l/attendenceapp/MainScreenFragment.java
@@ -0,0 +1,328 @@
+package com.example.arx8l.attendenceapp;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.os.CountDownTimer;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainScreenFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainScreenFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainScreenFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "service";
+    private static final long START_TIME_IN_MILLIS = 10800000;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    Button tapOut;
+    RelativeLayout classAtt;
+    LinearLayout countdownTimerLayout;
+    TextView countDownTimerText;
+    ImageView tapIn;
+
+    private long mTimeLeftInMillis;
+    private long timeUserTappedIn;
+    private boolean userTappedIn;
+
+
+    public MainScreenFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MainScreenFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MainScreenFragment newInstance(String param1, String param2) {
+        MainScreenFragment fragment = new MainScreenFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myFragmentView = inflater.inflate(R.layout.fragment_main_screen, container, false);
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("prefs", MODE_PRIVATE);
+        userTappedIn = prefs.getBoolean("userTappedIn", false);
+
+        countdownTimerLayout = myFragmentView.findViewById(R.id.countdown_timer_layout);
+
+        tapIn = getActivity().findViewById(R.id.tap_in_tap_out);
+
+        tapIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent getQrCodeResultIntent = new Intent(getContext(), QRCodeScanner.class);
+                startActivityForResult(getQrCodeResultIntent, 1);
+            }
+        });
+
+        tapOut = myFragmentView.findViewById(R.id.tap_out);
+        tapOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent getQrCodeResultIntent = new Intent(getContext(), QRCodeScanner.class);
+                startActivityForResult(getQrCodeResultIntent, 2);
+            }
+        });
+
+        countDownTimerText = new TextView(getContext());
+        countDownTimerText.setTextSize(24);
+
+        if (!userTappedIn){
+            tapOut.setVisibility(View.GONE);
+        }
+
+//        classAtt = myFragmentView.findViewById(R.id.class_att);
+//        classAtt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ClassAttendanceFragment classAttendanceFragment = new ClassAttendanceFragment();
+//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                fragmentTransaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+//                fragmentTransaction.replace(R.id.main_frag, classAttendanceFragment, "");
+//                fragmentTransaction.commit();
+//            }
+//        });
+
+//        String text = "Tapping Out in:";
+//        SpannableString ss = new SpannableString(text);
+//        ForegroundColorSpan fcsRed = new ForegroundColorSpan(Color.RED);
+//        ss.setSpan(fcsRed, 8, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        tappingOut.setText(ss);
+
+        if (userTappedIn){
+            countdownTimerLayout.addView(countDownTimerText);
+            getActivity().startService(new Intent(getContext(), BroadcastService.class));
+            Log.i(TAG, "Started service");
+        }
+
+        return myFragmentView;
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == 0 && data != null) {
+            String result = data.getStringExtra("cc");
+
+            if (result.equals("JCU QR Code Attendance")) {
+                alertMessage("Successfully Tapped In!");
+                timeUserTappedIn = System.currentTimeMillis();
+
+                countdownTimerLayout.addView(countDownTimerText);
+
+                getActivity().startService(new Intent(getActivity(), BroadcastService.class));
+                Log.i(TAG, "Started service");
+
+                userTappedIn = true;
+
+                SharedPreferences prefs = getActivity().getSharedPreferences("prefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+
+                editor.putBoolean("userTappedIn", userTappedIn);
+                editor.putLong("timeUserTappedIn", timeUserTappedIn);
+                editor.apply();
+
+            } else {
+                alertMessage("Error");
+            }
+        }
+        else if(requestCode == 2 && resultCode == 0 && data != null){
+            String result = data.getStringExtra("cc");
+            if (result.equals("JCU QR Code Attendance")) {
+                alertMessage("Successfully Tapped Out!");
+
+                SharedPreferences prefs = getActivity().getSharedPreferences("prefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                userTappedIn = false;
+                editor.putBoolean("userTappedIn", userTappedIn);
+                editor.apply();
+                tapOut.setVisibility(View.GONE);
+            } else {
+                alertMessage("Error");
+            }
+        }
+    }
+
+    private void alertMessage(String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+        alertDialog.setTitle("Scan result");
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateGUI(intent); // or whatever method used to update your GUI fields
+        }
+    };
+
+    private void updateGUI(Intent intent) {
+        if (intent.getExtras() != null) {
+            long millisUntilFinished = intent.getLongExtra("countdown", 0);
+            boolean timerFinished = intent.getBooleanExtra("timer finish", false);
+
+            String hms = String.format("Tapping Out in: " + "%02d:%02d:%02d",
+                    TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+            countDownTimerText.setText(hms);//set text
+
+            if(timerFinished){
+                countdownTimerLayout.removeView(countDownTimerText);
+                tapOut.setVisibility(View.VISIBLE);
+                getActivity().stopService(new Intent(getContext(), BroadcastService.class));
+                Log.i(TAG, "Stopped service");
+            }
+            else {
+                tapOut.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void onStop() {
+        try {
+            getActivity().unregisterReceiver(br);
+        } catch (Exception e) {
+            // Receiver was probably already stopped in onPause()
+        }
+
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+        getActivity().stopService(new Intent(getContext(), BroadcastService.class));
+        Log.i(TAG, "Stopped service");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(br, new IntentFilter(BroadcastService.COUNTDOWN_BR));
+        Log.i(TAG, "Registered broacast receiver");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(br);
+        Log.i(TAG, "Unregistered broacast receiver");
+    }
+}
