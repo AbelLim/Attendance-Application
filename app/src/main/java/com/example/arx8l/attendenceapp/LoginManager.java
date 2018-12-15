156ec6098da4f2c19aa7b460dbb2fbb95c98bf76
AbelLim <abel.lim@my.jcu.edu.au>
2018-12-16 01:20:32 +0800

    Clean up unused import statements.

--- app/src/main/java/com/example/arx8l/attendenceapp/LoginManager.java
+++ app/src/main/java/com/example/arx8l/attendenceapp/LoginManager.java
@@ -1,7 +1,6 @@
 package com.example.arx8l.attendenceapp;
 
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
--- app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
+++ app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
@@ -6,11 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textView;
