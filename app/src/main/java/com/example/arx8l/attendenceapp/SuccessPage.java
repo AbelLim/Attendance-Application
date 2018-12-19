9339b7d6b590ecd3632609f63dfae742720e1cf9
Vaibhav <vaibhav1andonlyone@yahoo.in>
2018-12-19 15:04:12 +0800

    Added Success Page and its files.

--- app/src/main/java/com/example/arx8l/attendenceapp/SuccessPage.java
+++ app/src/main/java/com/example/arx8l/attendenceapp/SuccessPage.java
@@ -8,6 +8,6 @@ public class SuccessPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Success_Layout);
+        setContentView(R.layout.success_layout);
     }
 }
