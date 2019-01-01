c405653b44649117f9bd131af8ac151c03ca165b
AbelLim <abel.lim@my.jcu.edu.au>
2019-01-02 01:50:25 +0800

    Add input to FileManager

--- app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
+++ app/src/main/java/com/example/arx8l/attendenceapp/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.arx8l.attendenceapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    private FileManager fm = new FileManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
-
 
     @Override
     protected void onDestroy() {
